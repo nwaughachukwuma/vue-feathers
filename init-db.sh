16bb1453ee028dc3c672879b2052b32cec14bce2
Max Gerer <max@e-bot7.com>
2018-04-21 11:54:15 +0200

    Give mongo some time to initialize

--- init-db.sh
+++ init-db.sh
@@ -2,6 +2,9 @@
 
 /usr/bin/mongod --fork --dbpath /data/db --logpath /dev/stdout --nojournal || exit 1
 
+# Give mongo some time to initialize
+sleep 5
+
 /usr/bin/mongo app init-db.js || exit 1
 
 /usr/bin/mongod --dbpath /data/db --shutdown
