83d70e6782353244f24a998c7fc07ab6c84682b0
matthew-bernardo <matthew@e-bot7.com>
2018-05-03 15:57:48 +0200

    BT-1414 add test description and global hook

--- srv/src/app.hooks.js
+++ srv/src/app.hooks.js
@@ -3,7 +3,12 @@ const logger = require('./hooks/logger');
 
 module.exports = {
   before: {
-    all: [],
+    all: [
+      // Do not edit - This introduces a delay to external calls intended to cause race conditions
+      (hook) => hook.params.provider
+        ? new Promise((resolve) => setTimeout(() => { resolve(hook) }, Math.random()*200))
+        : hook
+    ],
     find: [],
     get: [],
     create: [],
