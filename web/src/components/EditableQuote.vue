9d0bb6d08e07e7563d24d4a1c9256cbfbd3b2ffb
Chukwuma Nwaugha <c.nwaugha@gmail.com>
2019-11-16 21:23:03 +0100

    make editable quote component

--- web/src/components/EditableQuote.vue
+++ web/src/components/EditableQuote.vue
@@ -1,35 +1,67 @@
 <template>
-  <form @submit.prevent="onSubmit" v-if="mode==='edit'">
-    <div class="form-group">
-      <label for="exampleInputName">Author name</label>
-      <input type="text" class="form-control" id="exampleInputName"
-      placeholder="Name" v-model="name">
+  <li class="list-group-item">
+    <form @submit.prevent="onSubmit" v-if="displayMode==='edit'">
+        <div class="form-group">
+        <label for="inputName">Author name</label>
+        <input 
+            type="text" 
+            class="form-control" 
+            id="inputName"
+            placeholder="Name" 
+            v-model="name"
+            v-show="!disableAuthorField"
+        >
+        <input 
+            type="text" 
+            class="form-control" 
+            id="inputName"
+            :placeholder="authorQuote.author" 
+            v-model="name"
+            :disabled="true"
+            v-show="disableAuthorField"
+        >
+        </div>
+        <div class="form-group">
+        <label for="inputQuote">Quote</label>
+        <input 
+            type="text" 
+            class="form-control" 
+            id="inputQuote"
+            :placeholder="'Quote'" 
+            v-model="quote"
+        >
+        </div>
+        <button type="submit" class="btn btn-default">Save quote</button>
+        <button 
+            type="button" 
+            class="btn btn-warning" 
+            v-show="disableAuthorField"
+            @click="toggleMode"
+        >
+            Dismiss
+        </button>
+    </form>
+    <div @click="toggleMode" v-else>
+        <p >
+            <i class="fa fa-quote-left"></i>
+            {{ authorQuote.text }}
+        </p>
     </div>
-    <div class="form-group">
-      <label for="exampleInputQuote">Quote</label>
-      <input type="text" class="form-control" id="exampleInputQuote"
-      placeholder="Quote" v-model="quote">
-    </div>
-    <button type="submit" class="btn btn-default">Save quote</button>
-  </form>
-  <li class="list-group-item" v-else>
-    <p>
-      <i class="fa fa-quote-left"></i>
-      {{ authorQuote.text }}
-    </p>
   </li>
 </template>
 
 <script>
   export default {
     props: {
-      authorQuote: { type: Object, default: null },
+      authorQuote: { type: Object, default: () => ({text: ''}) },
       mode: { type: String, default: 'display'} // ['display', 'edit']
     },
     data () {
       return {
         name: '',
-        quote: ''
+        quote: '',
+        displayMode: '',
+        disableAuthorField: false
       }
     },
     methods: {
@@ -39,11 +71,27 @@
           text: this.quote
         })
           .then(() => this.$emit('create'))
+      },
+      toggleMode() {
+        this.displayMode =  this.displayMode === 'edit'? 'display': 'edit'
+
+        if (this.displayMode === 'edit') {
+            this.disableAuthorField = true
+        }
       }
     },
     watch: {
-        mode(val) {
-
+        mode: {
+            handler(val) {
+                this.displayMode = val;
+            },
+            immediate: true
+        },
+        authorQuote: {
+            handler(val) {
+                this.quote = val.text;
+            },
+            immediate: true
         }
     }
   }
