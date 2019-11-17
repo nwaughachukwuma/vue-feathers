9d33f2ace3a5f0480fcf04265be8e50b63306b39
Chukwuma Nwaugha <c.nwaugha@gmail.com>
2019-11-17 12:12:58 +0100

    search field for quote

---
+++ web/src/components/SearchQuote.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="input-group cust-input-group">
+        <input 
+            class="form-control" 
+            type="text" 
+            placeholder="Search" 
+            aria-label="Search"
+            @input="searchItems"
+        >
+        <div class="input-group-btn">
+        <button class="btn btn-default" type="button" disabled>
+            <i class="glyphicon glyphicon-search"></i>
+        </button>
+        </div>
+    </div>
+</template>
+
+<script>
+import {debounce} from 'lodash'
+export default {
+    methods: {
+        searchItems: debounce(function(evt) {
+        const query = evt.target.value
+        this.$emit('search', {query})
+      }, 1000),
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.cust-input-group {
+  margin-bottom: 1em;
+}
+</style>
