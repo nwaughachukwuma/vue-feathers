5968c83debae1095f6135805fb597f0debb8ce08
Chukwuma Nwaugha <c.nwaugha@gmail.com>
2020-01-22 15:16:01 +0100

    add redis for rate limiting on express middleware

--- srv/src/handlers/helpers.js
+++ srv/src/handlers/helpers.js
@@ -15,7 +15,7 @@ const getUniqueLetters = (str) => {
  * Deep diff between two object, using lodash
  * @param  {Object} object Object compared
  * @param  {Object} base   Object to compare with
- * @return {Object}        Return a new object who represent the diff
+ * @return {Object}        Return a new object representing the diff
  */
 const difference = (object, base) => {
 	function changes(object, base) {
--- srv/src/middleware/rate_limiting.js
+++ srv/src/middleware/rate_limiting.js
@@ -8,11 +8,13 @@ module.exports  = function() {
     const limiter = rateLimit({
       store: new RedisStore({
           // Configuration @https://github.com/wyattjoh/rate-limit-redis#configuration
-          // expiry: 60
+          // how long each rate limiting window exists for (in seconds)
+          expiry: 1 * 60,
           client: redisClient
       }),
       // milliseconds to keep records of requests in memory in minutes
-      windowMs: 5 * 60 * 1000,
+      // overriden by expiry when redis is used
+      //   windowMs: 5 * 60 * 1000,
       max: 100 // limit each IP to 100 requests per windowMs
     });
   
